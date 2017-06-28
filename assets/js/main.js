b03f8f70b75ce6b67efc70b2b7720da71e476686
caroquintana <caro.quintana.n@gmail.com>
2017-06-28 19:59:25 -0400

    avances en html css y  js

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(function() {
+  var listnum = 1;
+  var $input = $("#listItem"),
+      $list = $("#todo");
+  
+    function addListItem(e) {
+      if(e.type==="keydown" && e.which !== 13) return;
+      e.preventDefault(); // Don't submit
+      $list.append("<li><input type='checkbox' id='checkbox" + $input.val() +"' value='none' name='check'><label class='checkbox' for='checkbox" + $input.val()  + "'>" + $input.val() + "</label><input type='submit' id='eliminar' value='Remove'></li>"); 
+      $input.val(""); // Reset input
+    }
+    $("#listItem").keydown(addListItem);
+
+    $('.todo').on('click', '.eliminar',function(){
+     $(this).parent().remove();
+   });
+
+  });
+
